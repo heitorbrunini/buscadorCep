eb984c9c3a1cc82489793c82f78b7e4a4228e2f3
Heitor Brunini <68927093+heitorbrunini@users.noreply.github.com>
2023-06-21 19:35:48 -0300

    interface done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
+import { FiSearch } from 'react-icons/fi'
+import './styles.css'
 
 function App() {
   return (
     <div className="container">
-     <h1>
-      test
-     </h1>
+      <h1 className="title">
+        Buscador Cep
+      </h1>
+      <div className="container-input">
+        <input
+          type="text"
+          placeholder="digite um cep"
+        >
+        </input>
+        <button className="bt-search">
+          <FiSearch size={25} color='white' />
+        </button>
+      </div>
+
+      <main className='main'>
+        <h2>CEP: 52416255</h2>
+        <span>Rua visualizar</span>
+        <span>complemento</span>
+        <span>bairro visualizar</span>
+        <span>Campina Grande - PB</span>
+      </main>
+
+
     </div>
   );
 }
